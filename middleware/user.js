a7f2ac2dfdf0e69cea3db38fe802d5e70df4d4a4
Sumit <sumitchauhan9807666@gmail.com>
2020-03-30 21:51:52 +0530

    asd

--- middleware/user.js
+++ middleware/user.js
@@ -1,6 +1,6 @@
 const User = require('../models/user');
 exports.userData = (req,res,next)=>{
-    var userId =  "5e7f6a307cf4011ac821649c";
+    var userId =  "5e821c86d99be04a67062704";
     User.findOne({_id:userId}).exec((err,result)=>{
         req.userData = result
         next();
