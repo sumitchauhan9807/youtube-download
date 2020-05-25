cb74f47188e85de850ce8b83ede96101905f9995
Sumit Chauhan <sumitchauhan9807666@gmail.com>
2020-05-26 03:05:10 +0530

    asd

--- js/search_pagination.js
+++ js/search_pagination.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $(window).scroll(function() {
-        if($(window).scrollTop() + $(window).height() == $(document).height()) {
+        if($(window).scrollTop() + $(window).height()  == $(document).height()) {
             //alert("bottom!");
             if(window.nextPage){
                 $(".loading").css("display","block")
